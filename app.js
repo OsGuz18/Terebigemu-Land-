6343d3f148793c4068f946b004fb6155aa60f71f
OsGuz18 <guzmanmariscalosmar@gmail.com>
2021-06-09 12:03:58 -0500

    Intalación del package.json, de express, creación del esquema de carpetas e implementación del código para montar el servidor

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const publicPath = path.resolve(__dirname,'/public');
+
+app.use(express.static(publicPath));
+
+app.listen(3030, () => {
+    console.log('Servidor corriendo en el puerto 3030');
+});
+
+app.get("/",(req,res) => {
+    res.send("Hola Mundo");
+});
