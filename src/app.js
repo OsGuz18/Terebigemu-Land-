c79316a4539c0a1b50173171ddcaf006ebc6b329
OsGuz18 <guzmanmariscalosmar@gmail.com>
2021-07-06 20:19:13 -0500

    Reestructuración del árbol de trabajo

--- src/app.js
+++ src/app.js
@@ -3,13 +3,19 @@ const path = require("path");
 
 const app = express();
 
-const publicPath = path.resolve(__dirname,'./public');
+const publicPath = path.resolve(__dirname,'../public');
 
 app.use(express.static(publicPath));
+
+app.set("view engine","ejs")
+app.set("views",path.join(__dirname , "views") )
 
 app.listen(3030, () => {
     console.log('Servidor corriendo en el puerto 3030');
 });
+
+
+
 
 app.get("/",(req,res) => {
     res.sendFile(path.resolve(__dirname, './views/index.html'));
